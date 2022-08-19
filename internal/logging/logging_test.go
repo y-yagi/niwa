813d2ac335466823b597a38e2f51cb6956453575
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2022-08-19 17:12:53 +0900

    Add test for logging

---
+++ internal/logging/logging_test.go
@@ -0,0 +1,90 @@
+package logging_test
+
+import (
+	"os"
+	"path"
+	"testing"
+
+	"github.com/y-yagi/niwa/internal/logging"
+)
+
+func TestWrite(t *testing.T) {
+	tempDir, err := os.MkdirTemp("", "niwatest")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer os.RemoveAll(tempDir)
+
+	logfile := path.Join(tempDir, "niwa.log")
+	logger, err := logging.New(&logging.LogConfig{Output: "file", FilePath: logfile})
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	lf := logging.LogFormat{RemoteAddr: "192.168.1.1", TimeLocal: "2022/01/01 00:00", RequestMethod: "GET", ServerProtocol: "https", Status: 200, BodyBytesSent: 0, HttpReferer: "refer", HttpUserAgent: "dummy"}
+	if err = logger.Write(lf); err != nil {
+		t.Fatal(err)
+	}
+
+	log, err := os.ReadFile(logfile)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	wont := `192.168.1.1 [2022/01/01 00:00] "GET https" 200 0 "refer" "dummy"` + "\n"
+	if string(log) != wont {
+		t.Errorf("got: %s, wont: %s", log, wont)
+	}
+}
+
+func TestWrite_WithDiscard(t *testing.T) {
+	tempDir, err := os.MkdirTemp("", "niwatest")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer os.RemoveAll(tempDir)
+
+	logfile := path.Join(tempDir, "niwa.log")
+	logger, err := logging.New(&logging.LogConfig{Output: "discard", FilePath: logfile})
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	lf := logging.LogFormat{RemoteAddr: "192.168.1.1", TimeLocal: "2022/01/01 00:00", RequestMethod: "GET", ServerProtocol: "https", Status: 200, BodyBytesSent: 0, HttpReferer: "refer", HttpUserAgent: "dummy"}
+	if err = logger.Write(lf); err != nil {
+		t.Fatal(err)
+	}
+
+	if _, err = os.Stat(logfile); err == nil {
+		t.Errorf("expected log file doesn't exist, but it exists")
+	}
+}
+
+func TestWrite_WithFormat(t *testing.T) {
+	tempDir, err := os.MkdirTemp("", "niwatest")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer os.RemoveAll(tempDir)
+
+	logfile := path.Join(tempDir, "niwa.log")
+	logger, err := logging.New(&logging.LogConfig{Output: "file", FilePath: logfile, Format: "RemoteAddr: {{.RemoteAddr}}"})
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	lf := logging.LogFormat{RemoteAddr: "192.168.1.1", TimeLocal: "2022/01/01 00:00", RequestMethod: "GET", ServerProtocol: "https", Status: 200, BodyBytesSent: 0, HttpReferer: "refer", HttpUserAgent: "dummy"}
+	if err = logger.Write(lf); err != nil {
+		t.Fatal(err)
+	}
+
+	log, err := os.ReadFile(logfile)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	wont := "RemoteAddr: 192.168.1.1\n"
+	if string(log) != wont {
+		t.Errorf("got: %s, wont: %s", log, wont)
+	}
+}
